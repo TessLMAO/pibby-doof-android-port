d96069130ec81c42eca64c292a3e69e7f01c04a3
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-09-25 08:47:23 -0300

    Add files via upload

---
+++ assets/preload/custom_notetypes/GF Sing.lua
@@ -0,0 +1,17 @@
+function onCreate()
+	--Iterate over all notes
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'GF Sing' then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'HURTNOTE_assets'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.023'); --Default value is: 0.023, health gained on hit
+			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '0.0475'); --Default value is: 0.0475, health lost on miss
+			setPropertyFromGroup('unspawnNotes', i, 'hitCausesMiss', false);
+
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
+				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', true); --Miss has no penalties
+			end
+		end
+	end
+	--debugPrint('Script started!')
+end
+
